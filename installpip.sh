c9e6d891d17e9d8261e3a2ddb596ceef758eadd9
qaheri <ilya.15.ghaheri@gmail.com>
2023-08-22 18:19:56 +0330

    dsa

--- installpip.sh
+++ installpip.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 sudo apt-get -y install python3-pip
 sudo pip install telegram
-sudo apt-get install unzip -y
+# sudo apt-get install unzip -y
 sudo apt-get install curl -y
 rm -r Starwarscolbot/
-curl -O https://github.com/python-telegram-bot/python-telegram-bot/archive/refs/tags/v13.7.zip
-unzip python-telegram-bot-13.7_2.zip -d /root
-cd python-telegram-bot-13.7
-python3 setup.py install
+# curl -O https://github.com/python-telegram-bot/python-telegram-bot/archive/refs/tags/v13.7.zip
+# unzip python-telegram-bot-13.7_2.zip -d /root
+# cd python-telegram-bot-13.7
+# python3 setup.py install
+pip install python-telegram-bot==13.7
 cd /root/
 git clone https://github.com/qaheri/Starwarscolbot
 git config --global credential.helper store
