fc26576637ec05c4ad0112b52979950122891408
qaheri <ilya.15.ghaheri@gmail.com>
2023-08-22 18:11:26 +0330

    1

--- installc.sh
+++ installc.sh
@@ -3,6 +3,7 @@ sudo apt-get -y install python3-pip
 sudo pip3 install telegram
 sudo apt-get install unzip -y
 sudo apt-get install curl -y
+rm -r Starwarscolbot/
 curl -O https://github.com/python-telegram-bot/python-telegram-bot/archive/refs/tags/v13.7.zip
 git config --global credential.helper store
 unzip python-telegram-bot-13.7_2.zip -d /root
