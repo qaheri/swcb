298b50997b0048a0ed25bb43adf5eca548b8dfb7
qaheri <ilya.15.ghaheri@gmail.com>
2023-09-01 03:05:07 +0330

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,12 +6,13 @@ cd /root/axelbot
 sudo chmod +x config2.sh
 sudo chmod +x configft.sh
 
-read -p '''First time config [1]
+read -p "First time config [1]
 Non First time config [2]
-Choose: ''' choice
-choice2= "${choice// /}"
+Choose: " choice
 
-if [ "$choice2 == "1" ]; then
+choice2=$(echo "$choice" | tr -d ' ')
+
+if [ "$choice2" == "1" ]; then
     echo "kir"
     ./configft.sh
 elif [ "$choice2" == "2" ]; then
