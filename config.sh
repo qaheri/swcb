0241abc6d35071c3690ac8b112336ace8615b1b5
qaheri <ilya.15.ghaheri@gmail.com>
2023-09-01 02:51:11 +0330

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,4 +4,13 @@ git clone https://github.com/qaheri/axelbot
 git config --global credential.helper store
 cd axelbot
 sudo chmod +x config2.sh
-bash config2.sh
+sudo chmod +x configft.sh
+
+echo "First time config [1]
+Non First time config [2]"
+read choice
+
+if [ "$choice" == "1" ]; then
+    bash configft.sh
+elif [ "$choice" == "2" ]; then
+    bash config2.sh
