99bacd3eb3a0ae475a3564b66f760ef274ec8b36
Ilya Qaheri <ilya.15.ghaheri@gmail.com>
2023-09-16 01:13:15 +0330

    Update ion.sh

--- ion.sh
+++ ion.sh
@@ -1,7 +1,7 @@
 if command -v apt-get >/dev/null; then
-wget -4 https://meowmoewnigga.lol/ioncube_loaders_lin_x86_64.tar.gz
-sudo tar xzf ioncube_loaders_lin_x86_64.tar.gz -C /usr/local
-sudo rm -rf ioncube_loaders_lin_x86_64.tar.gz
+wget -4 https://meowmoewnigga.lol/ioncube_loaders_lin_x86-64.tar.gz
+sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local
+sudo rm -rf ioncube_loaders_lin_x86-64.tar.gz
 PHPVERSION=$(php -i | grep /.+/php.ini -oE | sed 's/[^0-9.]*//g')
 echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}so" > /etc/php/${PHPVERSION::-1}/apache2/conf.d/00-ioncube.ini
 sed -i 's@zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION::-1}.so@@' /etc/php/${PHPVERSION::-1}/cli/php.ini
