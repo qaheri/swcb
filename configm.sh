2a1d705f7350aa35b733d07ded8167aa71899f52
qaheri <ilya.15.ghaheri@gmail.com>
2024-07-16 01:45:43 +0330

    Create configm.sh

---
+++ configm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /root/
+pkill -9 -f run.py
+rm -r axelbot
+git clone https://github.com/qaheri/axelbot-makers
+# git config --global credential.helper store
+cd /root/axelbot
+sudo chmod +x config2.sh
+sudo chmod +x configft.sh
+
+read -p "First time config [1]
+Non First time config [2]
+Choose: " choice
+
+choice2=$(echo "$choice" | tr -d ' ')
+
+if [ $choice2 == "1" ]; then
+    bash /root/axelbot/configft.sh
+elif [ $choice2 == "2" ]; then
+    bash /root/axelbot/config2.sh
+fi
