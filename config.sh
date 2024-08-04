06f6936f6ec025ab2f6a9de7aa60f337e1f85f9f
qaheri <ilya.15.ghaheri@gmail.com>
2024-08-04 23:37:55 +0330

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ cd /root/
 pkill -9 -f run.py
 rm -r axelbot
 git clone https://github.com/qaheri/axelbot
-# git config --global credential.helper store
+git config --global credential.helper store
 cd /root/axelbot
 sudo chmod +x config2.sh
 sudo chmod +x configft.sh
