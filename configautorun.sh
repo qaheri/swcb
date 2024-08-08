55963234ba2d46fd734ec3fed50edb4c567b4012
qaheri <ilya.15.ghaheri@gmail.com>
2024-08-08 15:55:56 +0330

    Create configautorun.sh

---
+++ configautorun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /root/
+pkill -9 -f run.py
+rm -r axelbot
+git clone https://github.com/qaheri/axelbot
+git config --global credential.helper store
+cd /root/axelbot
+sudo chmod +x config2.sh
+sudo chmod +x configft.sh
+
+bash /root/axelbot/autorun.sh
