bf76b0d09d824d94456b940827057639e114e8cd
Ilya Qaheri <ilya.15.ghaheri@gmail.com>
2024-08-24 10:59:29 +0330

    Update gost_c.sh

--- gost_c.sh
+++ gost_c.sh
@@ -5,8 +5,8 @@ temp_dir=$(mktemp -d)
 cd "$temp_dir" || exit
 wget https://github.com/qaheri/swcb/raw/main/gost_i_c.sh
 sudo mkdir -p /etc/gost
-sudo mv install.sh /etc/gost/
-sudo chmod +x /etc/gost/install.sh
+sudo mv gost_i_c.sh /etc/gost/
+sudo chmod +x /etc/gost/gost_i_c.sh
 cd /etc/gost || exit
-sudo ./install.sh
+sudo ./gost_i_c.sh
 rm -rf "$temp_dir"
