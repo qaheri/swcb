20a1e5be59ddf1fe3f3941c09a22fb8fa9b4074b
qaheri <ilya.15.ghaheri@gmail.com>
2025-02-20 22:10:23 +0330

    Create configs.sh

---
+++ configs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /root/
+pkill -9 -f run.py
+datee=$(date +"%Y-%m-%d_%T")
+mv /root/axelbot /root/releases/axelbot_$datee
+# rm -r axelbot
+git clone https://github.com/qaheri/autselfbot
+mv /root/autselfbot /root/axelbot
+git config --global credential.helper store
+cd /root/axelbot
+sudo chmod +x config2.sh
+sudo chmod +x configft.sh
+
+read -p "First time config [1]
+Non First time config [2]
+Choose: " choice
+
+choice2=$(echo "$choice" | tr -d ' ')
+
+if [ $choice2 == "1" ]; then
+    bash /root/axelbot/configft.sh
+elif [ $choice2 == "2" ]; then
+    bash /root/axelbot/config2.sh
+fi
